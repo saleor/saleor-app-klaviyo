f9d4d80a45c4555164efdfbd8c9a5637c3fc8cc0
Tomasz Magulski <tomasz.magulski@saleor.io>
2022-05-09 13:50:39 +0200

    [SALEOR-6636] Webhooks' definitions in manifest, part 2

--- pages/api/manifest.ts
+++ pages/api/manifest.ts
@@ -1,7 +1,14 @@
 import { NextApiHandler } from "next";
+import { print } from "graphql/language/printer";
 
 import { version, name } from "../../package.json";
 import { getBaseURL } from "../../lib/middlewares";
+import {
+  CustomerCreatedSubscriptionDocument,
+  FulfillmentCreatedSubscriptionDocument,
+  OrderCreatedSubscriptionDocument,
+  OrderFullyPaidSubscriptionDocument,
+} from "../../generated/graphql";
 
 const handler: NextApiHandler = async (request, response) => {
   const baseURL = getBaseURL(request);
@@ -10,9 +17,39 @@ const handler: NextApiHandler = async (request, response) => {
     id: "saleor.app.klaviyo",
     version: version,
     name: name,
-    permissions: ["MANAGE_ORDERS"],
+    permissions: ["MANAGE_USERS", "MANAGE_ORDERS"],
     configurationUrl: `${baseURL}/configuration`,
     tokenTargetUrl: `${baseURL}/api/register`,
+    webhooks: [
+      {
+        name: "order-created",
+        events: ["ORDER_CREATED"],
+        query: print(OrderCreatedSubscriptionDocument),
+        targetUrl: `${baseURL}/api/webhooks/order-created`,
+        isActive: true,
+      },
+      {
+        name: "order-fully-paid",
+        events: ["ORDER_FULLY_PAID"],
+        query: print(OrderFullyPaidSubscriptionDocument),
+        targetUrl: `${baseURL}/api/webhooks/order-fully-paid`,
+        isActive: true,
+      },
+      {
+        name: "customer-created",
+        events: ["CUSTOMER_CREATED"],
+        query: print(CustomerCreatedSubscriptionDocument),
+        targetUrl: `${baseURL}/api/webhooks/customer-created`,
+        isActive: true,
+      },
+      {
+        name: "fulfillment-created",
+        events: ["FULFILLMENT_CREATED"],
+        query: print(FulfillmentCreatedSubscriptionDocument),
+        targetUrl: `${baseURL}/api/webhooks/fulfillment-created`,
+        isActive: true,
+      },
+    ],
   };
 
   response.json(manifest);
