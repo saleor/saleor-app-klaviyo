36e5f966fb7eafe18998e00ee4c4e06f4df7b2d4
Krzysztof Wolski <krzysztof.wolski@saleor.io>
2023-01-18 11:32:32 +0100

    Implement new SDK features

--- pages/api/webhooks/order-created.ts
+++ pages/api/webhooks/order-created.ts
@@ -1,13 +1,9 @@
-import { SALEOR_API_URL_HEADER } from "@saleor/app-sdk/const";
-import { SaleorAsyncWebhook } from "@saleor/app-sdk/handlers/next";
-import { withSentry } from "@sentry/nextjs";
-import type { Handler } from "retes";
-import { toNextHandler } from "retes/adapter";
-import { Response } from "retes/response";
+import { NextWebhookApiHandler, SaleorAsyncWebhook } from "@saleor/app-sdk/handlers/next";
 import { gql } from "urql";
 
+import { createClient } from "../../../lib/graphql";
 import Klaviyo from "../../../lib/klaviyo";
-import { getValue } from "../../../lib/metadata";
+import { createSettingsManager } from "../../../lib/metadata";
 import { saleorApp } from "../../../saleor-app";
 
 export const orderCreatedWebhook = new SaleorAsyncWebhook<unknown>({
@@ -24,33 +20,38 @@ export const orderCreatedWebhook = new SaleorAsyncWebhook<unknown>({
   `,
 });
 
-const handler: Handler = async (request) => {
-  const saleorApiUrl = request.headers[SALEOR_API_URL_HEADER] as string;
-  const klaviyoToken = await getValue(saleorApiUrl, "PUBLIC_TOKEN");
-  const klaviyoMetric = await getValue(saleorApiUrl, "ORDER_CREATED_METRIC");
-  const context = request.params;
-  const { userEmail } = context.order;
+const handler: NextWebhookApiHandler<any> = async (req, res, context) => {
+  const { payload, authData } = context;
+  const { saleorApiUrl, token, appId } = authData;
+  const client = createClient(saleorApiUrl, async () => Promise.resolve({ token }));
+  const settings = createSettingsManager(client, appId);
+
+  const klaviyoToken = await settings.get("PUBLIC_TOKEN");
+  const klaviyoMetric = await settings.get("CUSTOMER_CREATED_METRIC");
+  if (!klaviyoToken || !klaviyoMetric) {
+    return res.status(400).json({ success: false, message: "App not configured." });
+  }
+
+  const { userEmail } = payload.order;
 
   if (!userEmail) {
-    return Response.BadRequest({ success: false, message: "No user email." });
+    return res.status(400).json({ success: false, message: "No user email." });
   }
 
   const klaviyoClient = Klaviyo(klaviyoToken);
-  const klaviyoResponse = await klaviyoClient.send(klaviyoMetric, userEmail, context);
+  const klaviyoResponse = await klaviyoClient.send(klaviyoMetric, userEmail, payload);
 
   if (klaviyoResponse.status !== 200) {
     const klaviyoMessage = ` Message: ${(await klaviyoResponse.json())?.message}.` || "";
-    return Response.InternalServerError({
+    return res.status(500).json({
       success: false,
       message: `Klaviyo API responded with status ${klaviyoResponse.status}.${klaviyoMessage}`,
     });
   }
-  return Response.OK({ success: true, message: "Message sent!" });
+  return res.status(200).json({ success: true, message: "Message sent!" });
 };
 
-const wrappedHandler = withSentry(toNextHandler([handler]));
-
-export default orderCreatedWebhook.createHandler(wrappedHandler);
+export default orderCreatedWebhook.createHandler(handler);
 
 export const config = {
   api: {
